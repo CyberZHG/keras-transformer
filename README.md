a7714d30daf6c1bc3d8fd836b439b583d056fdad
CyberZHG <CyberZHG@gmail.com>
2018-10-27 13:37:00 +0800

    Initialize framework

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# keras-transformer
+# Keras Transformer
+
+[![Travis](https://travis-ci.org/CyberZHG/keras-transformer.svg)](https://travis-ci.org/CyberZHG/keras-transformer)
+[![Coverage](https://coveralls.io/repos/github/CyberZHG/keras-transformer/badge.svg?branch=master)](https://coveralls.io/github/CyberZHG/keras-transformer)
+
+## Install
+
+```bash
+pip install keras-transformer
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keras-transformer',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/CyberZHG/keras-transformer',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Transformer implemented in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+        'keras-multi-head==0.7.0',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
