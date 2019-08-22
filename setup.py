4d9b34f95fcc6680fa43ae33ea7e2c6579c3efda
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-08-23 07:16:44 +0800

    Fix compatibility issues with Keras 2.2.5

--- setup.py
+++ setup.py
@@ -12,19 +12,18 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 setup(
     name='keras-transformer',
-    version='0.29.0',
+    version='0.30.0',
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-transformer',
     license='MIT',
     author='CyberZHG',
-    author_email='CyberZHG@gmail.com',
+    author_email='CyberZHG@users.noreply.github.com',
     description='Transformer implemented in Keras',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=install_requires,
     classifiers=(
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
