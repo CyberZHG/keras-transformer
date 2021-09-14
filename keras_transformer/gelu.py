58f2b907c127d636e9b527f3e2210567278d32ff
@BenjaminWegener <wegeneredv@gmail.com>
2021-09-14 17:38:55 +0200

    Update gelu.py
    replaced ´math.sqrt( 2.0 / math.pi )´ with the approximation 0.7978845608028654 to save some op/s

--- keras_transformer/gelu.py
+++ keras_transformer/gelu.py
@@ -7,4 +7,4 @@ def gelu(x):
 
     See: https://arxiv.org/pdf/1606.08415.pdf
     """
-    return 0.5 * x * (1.0 + K.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * x * x * x)))
+    return 0.5 * x * (1.0 + K.tanh(0.7978845608028654 * (x + 0.044715 * x * x * x))) #save some ops -> math.sqrt(2.0 / math.pi) = 0.7978845608028654
